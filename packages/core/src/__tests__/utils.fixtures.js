962a8f8161bce07767fd7e3f2889100dbb40e902
Timofey Dergachev <t@exeto.me>
2019-05-03 12:41:05 +0300

    Add tests for utils

---
+++ packages/core/src/__tests__/utils.fixtures.js
@@ -0,0 +1,64 @@
+import { NOT_FOUND } from '../consts';
+
+export const notFoundRoute = {
+  keys: [],
+  path: null,
+  regexp: null,
+  type: NOT_FOUND,
+};
+
+export const simpleRoute = {
+  keys: [],
+  path: '/',
+  regexp: /^\/$/i,
+  type: 'router/HOME',
+};
+
+export const withParamsRoute = {
+  keys: [
+    {
+      delimiter: '/',
+      name: 'id',
+      optional: false,
+      pattern: '[^\\/]+?',
+      prefix: '/',
+      repeat: false,
+    },
+  ],
+  path: '/posts/:id',
+  // eslint-disable-next-line no-useless-escape
+  regexp: /^\/posts\/([^\/]+?)$/i,
+  type: 'router/POST',
+};
+
+export const rawRoutes = [notFoundRoute, simpleRoute, withParamsRoute];
+
+export const queryParamsString = '?sort=name&filters[age][eq]=25';
+
+export const queryParams = {
+  sort: 'name',
+  filters: {
+    age: {
+      eq: '25',
+    },
+  },
+};
+
+export const createHistory = (pathname, search = '') => ({
+  location: {
+    pathname,
+    search,
+  },
+});
+
+export const createRoutes = () => ({
+  getByPathname(pathname) {
+    const mapping = {
+      '/users/300': notFoundRoute,
+      '/': simpleRoute,
+      '/posts/42': withParamsRoute,
+    };
+
+    return mapping[pathname];
+  },
+});
