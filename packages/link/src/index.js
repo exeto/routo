4fee83fc112a6189cede6db565c938835f52fd22
Timofey Dergachev <t@exeto.me>
2020-01-07 16:00:49 +0000

    Use hooks for redux and add forwardRef for Link

--- packages/link/src/index.js
+++ packages/link/src/index.js
@@ -1,8 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import { createToPath } from '@routo/core';
+import { useSelector, useDispatch } from 'react-redux';
 
-import withData from './withData';
+import { getPathname } from './selectors';
 import { clsx } from './utils';
 
 export const createLink = routes => {
@@ -13,12 +14,10 @@ export const createLink = routes => {
     return toPath(params);
   };
 
-  const Link = props => {
+  const Link = forwardRef(function Link(props, ref) {
     const {
       to,
-      pathname,
       children,
-      dispatch,
       className,
       activeClassName,
       component,
@@ -27,6 +26,8 @@ export const createLink = routes => {
 
     const Component = component;
     const href = getUrl(to.type, to.payload);
+    const dispatch = useDispatch();
+    const pathname = useSelector(getPathname);
     const activeClass = href === pathname ? activeClassName : null;
 
     const handleClick = useCallback(
@@ -44,6 +45,7 @@ export const createLink = routes => {
 
     return (
       <Component
+        ref={ref}
         href={href}
         className={clsx(className, activeClass)}
         onClick={handleClick}
@@ -51,16 +53,14 @@ export const createLink = routes => {
         {children}
       </Component>
     );
-  };
+  });
 
   Link.propTypes = {
     to: PropTypes.shape({
       type: PropTypes.string.isRequired,
       payload: PropTypes.object,
     }).isRequired,
-    pathname: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
-    dispatch: PropTypes.func.isRequired,
     className: PropTypes.string,
     activeClassName: PropTypes.string,
     component: PropTypes.elementType,
@@ -74,5 +74,5 @@ export const createLink = routes => {
     onClick: () => {},
   };
 
-  return withData(Link);
+  return Link;
 };
--- packages/link/src/withData.js
+++
@@ -1,7 +0,0 @@
-import { connect } from 'react-redux';
-
-import { getPathname } from './selectors';
-
-const mapStateToProps = state => ({ pathname: getPathname(state) });
-
-export default connect(mapStateToProps);
