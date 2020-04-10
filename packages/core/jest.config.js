6df8c850af2e67520ad18b6335908fdedc75fedf
Timofey Dergachev <t@exeto.me>
2020-04-10 11:03:09 +0300

    Fix jest config

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -3,6 +3,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@routo/(.*)$': '<rootDir>/../$1/src',
+  },
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/__tests__/**/*.ts',
--- packages/link/jest.config.js
+++ packages/link/jest.config.js
@@ -2,6 +2,9 @@
 
 module.exports = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    '^@routo/(.*)$': '<rootDir>/../$1/src',
+  },
   collectCoverageFrom: [
     'src/**/*.ts?(x)',
     '!src/**/__tests__/**/*.ts?(x)',
